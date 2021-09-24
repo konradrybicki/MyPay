69990b8f53178d458cd634f2860fbd9bd992b7ef
Konrad Rybicki <konradrybicki.altamoda@gmail.com>
2021-09-24 14:03:14 +0200

    Account top-up controller implementation completed

--- MyPay/Controller/TopUpFormViewController.swift
+++ MyPay/Controller/TopUpFormViewController.swift
@@ -16,7 +16,7 @@ class TopUpFormViewController: UIViewController {
     @IBOutlet weak var amountTextField: UITextField!
     @IBOutlet weak var amountLabel: UILabel!
     @IBOutlet weak var amountTextFieldUnderline: UIView!
-    var isAmountValid = true
+    var amountValid = true
     
     // user's account balance
     @IBOutlet weak var balance: UILabel!
@@ -42,7 +42,7 @@ class TopUpFormViewController: UIViewController {
         // amount text field set-up
         
         amountTextField.delegate = self
-        amountTextField.addTarget(self, action: #selector(<#T##@objc method#>), for: .editingChanged)
+        amountTextField.addTarget(self, action: #selector(amountTextFieldDidChangeEditing), for: .editingChanged)
         
         // users account balance display
         
@@ -114,5 +114,127 @@ class TopUpFormViewController: UIViewController {
                 self.hideLoadingAnimation()
             }
         }
+    }
+}
+
+//MARK: - UITextFieldDelegate
+
+extension TopUpFormViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        // amount field blue mark
+        paintAmountCell(with: #colorLiteral(red: 0.2039215686, green: 0.5960784314, blue: 0.8588235294, alpha: 1), and: #colorLiteral(red: 0.2039215686, green: 0.5960784314, blue: 0.8588235294, alpha: 1))
+    }
+    
+    /// Reacts to an amountTextField's text change event
+    
+    @objc func amountTextFieldDidChangeEditing() {
+        
+        // potential amount field/validness state change
+        
+        let amount_updated = amountTextField.text!
+        
+        if amount_updated != "" {
+            
+            // validation
+            let amount_updated_valid = ValidationService.isTopUpAmountValid(Double(amount_updated)!)
+            
+            // result (became invalid)
+            if amountValid == true && amount_updated_valid == false {
+                
+                paintAmountCell(with: #colorLiteral(red: 0.9058823529, green: 0.2980392157, blue: 0.2352941176, alpha: 1), and: #colorLiteral(red: 0.9058823529, green: 0.2980392157, blue: 0.2352941176, alpha: 1))
+                amountValid = false
+            }
+            // result (became valid)
+            else if amountValid == false && amount_updated_valid == true {
+                
+                paintAmountCell(with: #colorLiteral(red: 0.2039215686, green: 0.5960784314, blue: 0.8588235294, alpha: 1), and: #colorLiteral(red: 0.2039215686, green: 0.5960784314, blue: 0.8588235294, alpha: 1))
+                amountValid = true
+            }
+        }
+        else {
+            
+            // invalid became empty
+            if amountValid == false {
+                
+                paintAmountCell(with: #colorLiteral(red: 0.2039215686, green: 0.5960784314, blue: 0.8588235294, alpha: 1), and: #colorLiteral(red: 0.2039215686, green: 0.5960784314, blue: 0.8588235294, alpha: 1))
+                amountValid = true
+            }
+        }
+        
+        // proceed button potential state change
+        
+        if proceedButtonLocked == true {
+            
+            // checking for potential unlock
+            
+            if amountValid == true {
+                
+                unlockProceedButton()
+                proceedButtonLocked = false
+            }
+        }
+        else {
+            
+            // checking for potential lock
+            
+            if amountFieldInvalidOrEmpty() == true {
+                
+                lockProceedButton()
+                proceedButtonLocked = true
+            }
+        }
+    }
+}
+
+//MARK: - Cell painting method
+
+extension TopUpFormViewController {
+    
+    /// Changes the amount cell's label and underline colors to given ones
+    
+    func paintAmountCell(with labelColor: UIColor, and underlineColor: UIColor) {
+        
+        UIView.animate(withDuration: 0.2) {
+            self.amountLabel.textColor = labelColor
+            self.amountTextFieldUnderline.backgroundColor = underlineColor
+        }
+    }
+}
+
+//MARK: - Proceed button manipulation methods
+
+extension TopUpFormViewController {
+    
+    /// Checks if the amount field is either invalid or empty
+    
+    func amountFieldInvalidOrEmpty() -> Bool {
+        
+        let amountFieldInvalid: Bool = (amountValid == false)
+        let amountFieldEmpty: Bool = (amountTextField.text! == "")
+        
+        if amountFieldInvalid || amountFieldEmpty {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    
+    /// Enables user interaction on the proceed button, changing its color simultaneously
+    
+    func unlockProceedButton() {
+        proceedButton.backgroundColor = #colorLiteral(red: 0.2039215686, green: 0.5960784314, blue: 0.8588235294, alpha: 1)
+        proceedButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        proceedButton.isUserInteractionEnabled = true
+    }
+
+    /// Disables user interaction on the proceed button, changing its color simultaneously
+    
+    func lockProceedButton() {
+        proceedButton.backgroundColor = #colorLiteral(red: 0.1607843137, green: 0.5019607843, blue: 0.7254901961, alpha: 1)
+        proceedButton.setTitleColor(#colorLiteral(red: 0.7411764706, green: 0.7647058824, blue: 0.7803921569, alpha: 1), for: .normal)
+        proceedButton.isUserInteractionEnabled = false
     }
 }
